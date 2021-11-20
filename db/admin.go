7f73e39ae7325e3aaebfd09106b2141bf0b17c3c
alterra_day_18 <anisaselena09@gmail.com>
2021-11-20 18:38:00 +0700

    relasi table

--- db/admin.go
+++ db/admin.go
@@ -7,3 +7,11 @@ type Admins struct {
 	Password string `json:"password" form:"password"`
 	//Token    string `json:"token" form:"token"`
 }
+
+type AdminRes struct {
+	ID    int    `json:"id" form:"id"`
+	Name  string `json:"name" form:"name"`
+	Email string `json:"email" form:"email"`
+	//Password string `json:"password" form:"password"`
+	Token string `json:"token,omitempty"`
+}
