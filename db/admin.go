be90f206d24dd4bf7635dc4679103bf79d89c503
alterra_day_18 <anisaselena09@gmail.com>
2021-11-24 07:38:10 +0700

    db admin

--- db/admin.go
+++ db/admin.go
@@ -1,17 +1,17 @@
-// package db
+package db
 
-// type Admins struct {
-// 	ID       int    `json:"id" form:"id"`
-// 	Name     string `json:"name" form:"name"`
-// 	Email    string `json:"email" form:"email"`
-// 	Password string `json:"password" form:"password"`
-// 	//Token    string `json:"token" form:"token"`
-// }
+type Admins struct {
+	ID       int    `json:"id" form:"id"`
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+	//Token    string `json:"token" form:"token"`
+}
 
-// type AdminRes struct {
-// 	ID    int    `json:"id" form:"id"`
-// 	Name  string `json:"name" form:"name"`
-// 	Email string `json:"email" form:"email"`
-// 	//Password string `json:"password" form:"password"`
-// 	Token string `json:"token,omitempty"`
-// }
+type AdminRes struct {
+	ID    int    `json:"id" form:"id"`
+	Name  string `json:"name" form:"name"`
+	Email string `json:"email" form:"email"`
+	//Password string `json:"password" form:"password"`
+	Token string `json:"token,omitempty"`
+}
--- middleware/auth_middleware.go
+++ middleware/auth_middleware.go
@@ -1,18 +1,18 @@
-// package middleware
+package middleware
 
-// import (
-// 	"project_backend/config"
-// 	"project_backend/db"
+import (
+	"project_backend/config"
+	"project_backend/db"
 
-// 	"github.com/labstack/echo/v4"
-// )
+	"github.com/labstack/echo/v4"
+)
 
-// func AuthDB(adminname, password string, c echo.Context) (bool, error) {
-// 	var admin db.Admins
+func AuthDB(adminname, password string, c echo.Context) (bool, error) {
+	var admin db.Admins
 
-// 	err := config.DB.Where("email = ? AND password = ?", adminname, password).First(&admin).Error
-// 	if err != nil {
-// 		return false, err
-// 	}
-// 	return true, nil
-// }
+	err := config.DB.Where("email = ? AND password = ?", adminname, password).First(&admin).Error
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
